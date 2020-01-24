01bf744f544cb2cf4f49399280d148ea35595632
priyam0001 <50718425+priyam0001@users.noreply.github.com>
2020-01-23 21:06:47 -0500

    completed lab3

---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+       
+
+        int PhoneId = 0;
+        
+        public Phone()
+        {
+            
+            PhoneName="";
+            Manufacturer="";
+        
+            DateRelease=new DateTime();
+
+        }
+        public int PhoneID { get; set; }
+        public String PhoneName { get; set; }
+
+        public String Manufacturer { get; set; }
+        public float MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateRelease { get; set; }
+    }
+}
